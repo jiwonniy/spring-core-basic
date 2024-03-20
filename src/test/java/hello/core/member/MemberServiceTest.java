e57c37c77048ed3e788bd11dd2b6f000fee90b6b
jiwonniy <jiwoni0707@gmail.com>
2024-03-20 21:20:31 +0900

    Test: Member and Order(Discount) test

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    //단위 테스트
+    @Test
+    void join(){
+
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then (검증)
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
