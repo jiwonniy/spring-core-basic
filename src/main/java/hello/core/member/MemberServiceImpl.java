3b6cfe627acf86f7b126cf8e79fe111d92a72b58
jiwonniy <jiwoni0707@gmail.com>
2024-03-25 23:05:30 +0900

    Refactor: 생성자 주입으로 변경

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,16 @@ public class MemberServiceImpl implements MemberService{
 
     //final : 객체 변경을 강제할 수 있는 final 키워드의 장점을 가져가면서 의존관계 주입을 사용할 수 있기 때문에
     //final 키워드를 사용한 생성자 주입을 권장하여 사용한 것
-    private  final MemberRepository memberRepository = new MemoryMemberRepository(); // 실제 할당하는 부분이 구현체를 의존함.. 추상화에도 의존, 구체화에도 의존 DIP 를 위반하고 있다.
+    //private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    //=> 문제점: 실제 할당하는 부분이 구현체를 의존함.. 추상화에도 의존, 구체화에도 의존 DIP 를 위반하고 있다.
+
+    //MemberRepository의 구현체가 뭐가 들어갈지를 생성자를 통해서 결정
+    //여기 어디에도 MemoryMemberRepository가 없음. **생성자 주입**
+    private  final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public  void join(Member member){
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,33 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private  final MemberRepository memberRepository = new MemoryMemberRepository();
-    private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //고정할인금액
+    //=> 문제점: FixDiscountPolicy (구체화)에 의존하고 있음, DiscountPolicy(추상화)에만 의존하고 있었던 것이 아님.
+    //=> 문제점: 따라서 DIP 위반
+
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //10%할인
+    //=> 문제점, 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 위와 같이 갈아 끼워야됨.
+    //=> OCP, DIP 준수를 안하고 있음
+
+    //추상화에 의존하도록 변경!
+    //MemoryMemberRepository와 FixDiscountPolicy 객체의 의존관계가 주입된다.
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+    //=> NPE 발생하므로, 누군가가 DiscountPolicy의 구현 객체를 대신 생성하고 주입해줘야한다!
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
